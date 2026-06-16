4736b5e181958ccb6460c1e33402dc1f3ae08c8b
kirubakaran182204 <vijayarajkirubakaran@gmail.com>
2026-06-16 10:52:57 +0530

    /

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Router, Routes, Route, HashRouter } from 'react-router-dom';
+import { HashRouter, Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import Home from './pages/Home.jsx';
